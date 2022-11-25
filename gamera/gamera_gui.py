43e645379ef802418d4ae17bffb6772b1b2bd0ee
Olaf Braun <info@braun-development.de>
2022-11-25 10:15:33 +0100

    Fix colored message end

--- gamera/gamera_gui.py
+++ gamera/gamera_gui.py
@@ -8,7 +8,7 @@ def gamera_gui():
 	try:
 		from wx import aui
 	except ImportError:
-		print(f"\033[91m Error: 'gamera_gui' need wxPython installed. Please install it, throw 'pip install wxPython'")
+		print(f"\033[91m Error: 'gamera_gui' need wxPython installed. Please install it, throw 'pip install wxPython'\033[0m")
 		exit(-1)
 
 	try:
